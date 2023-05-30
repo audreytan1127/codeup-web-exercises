a6cfb8301981f02112859db5cc55201765057e63
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-05-30 11:51:59 -0500

    created new js files and added script

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// alert("Wow.")
+//
+// const userResponse = confirm("are you hungry?");
+//console.log(userResponse);
+
+//const userResponse = prompt("What's your favorite food?")
+//console.log(userResponse)
