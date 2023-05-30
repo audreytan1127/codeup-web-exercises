e2db65d9d4751e549b47c2609437b80117620615
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-05-30 15:51:51 -0500

    added function exercises

---
+++ js/function-exercise.js
@@ -0,0 +1,51 @@
+"use strict"
+//function with parameters
+//function sayHello(usersUsername){
+  //  console.log("hiya " + usersUsername)
+//}
+//invoking with an argument
+//sayHello("meep");
+//sayHello("audrey");
+
+//refactor to use arguments and parameters
+//function sayBye(usersUsername) {
+ //   console.log("baiiiii! " + usersUsername)
+//}
+//sayBye("audrey");
+
+//function with return
+//function sayBye(usersUsername) {
+  //  return "baiiiii! " + usersUsername
+//}
+//const functionResp =sayBye("audrey");
+//console.log(functionResp)
+
+//global scope example
+
+// const username = "bobby"
+// function sayBye(){
+//     console.log ("baiiiii! " + username);
+// }
+// sayBye();
+// console.log(username)
+
+//local scope
+
+
+// function sayBye(){
+//     const username = "bobby"
+//     console.log ("baiiiii! " + username);
+// }
+// sayBye();
+//
+// console.log(username)
+
+// function addFourDivideByTwo(num) {
+//     const sum = num + 4;
+//     const dividedByTwo = dividedByTwo(sum)
+//     return dividedByTwo
+// }
+// function dividedByTwo(num) {
+//     return num / 2;
+// }
+// console.log(addFourDivideByTwo(5));
