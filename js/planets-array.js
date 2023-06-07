a3d0d50757392805ec42be97249ead8b174a0c43
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-06-07 10:46:13 -0500

    completed array manipulation exercise

---
+++ js/planets-array.js
@@ -0,0 +1,54 @@
+// (function(){
+    "use strict";
+
+    var planets = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ];
+
+    /**
+     * TODO:
+     * Read each console log below, and write some javascript code to perform
+     * the step that it describes
+     */
+
+    console.log('Adding "The Sun" to the beginning of the planets array.');
+    const addTheSunToPlanets = planets.unshift("Sun,");
+    console.log(addTheSunToPlanets);
+    console.log(planets);
+
+    console.log('Adding "Pluto" to the end of the planets array.');
+    const addPlutoToPlanets = planets.push("Pluto");
+    console.log(addPlutoToPlanets);
+    console.log(planets);
+
+    console.log('Removing "The Sun" from the beginning of the planets array.');
+    const removeSunFromPlanets = planets.shift();
+    console.log(removeSunFromPlanets);
+    console.log(planets);
+
+    console.log('Removing "Pluto" from the end of the planets array.');
+    const removePlutoFromPlanets = planets.pop();
+console.log(removePlutoFromPlanets);
+console.log(planets);
+
+    console.log('Finding and logging the index of "Earth" in the planets array.');
+const findIndexOfEarth = planets.indexOf("Earth");
+console.log(findIndexOfEarth);
+
+    console.log("Reversing the order of the planets array.");
+    const reverseOrderOfPlanets = planets.reverse();
+console.log(reverseOrderOfPlanets);
+console.log(planets);
+
+    console.log("Sorting the planets array.");
+    const sortPlanets = planets.sort();
+console.log(sortPlanets);
+console.log(planets);
+// })();
