a74f3b5f8faaeaad751fec09dbe2a7dd5b3551e1
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-07-07 14:00:13 -0500

    created jquery exercises html and js with jquery function to alert window has loaded.

---
+++ js/jquery_exercises.js
@@ -0,0 +1,9 @@
+// JS function alerts when the page finishes loading.
+// window.onload = function() {
+//     alert("The window has loaded");
+// }
+
+//jQuery alerts when page finishes loading
+$(function() {
+    alert("The window has loaded.");
+});
