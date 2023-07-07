fdd928f6c75f96e0bf4001dc287eafdb367a0a3f
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-07-07 15:19:07 -0500

    completed jQuery exercises on codeup curriculum.

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,6 +4,53 @@
 // }
 
 //jQuery alerts when page finishes loading
-$(function() {
-    alert("The window has loaded.");
-});
+// $(function() {
+//     alert("The window has loaded.");
+// });
+
+//jQuery arrow function
+// $(()=> {
+//     alert("Window has loaded!");
+// });
+
+//jQuery select id to change css
+// $(() => {
+//     $('#id-container').css('background-color', 'red');
+// });
+//
+// // jQuery class selector
+// $(".title").css('color', 'purple');
+//
+// //vanilla JS query select all title classes
+// let titleElements = document.querySelectorAll('.title');
+// titleElements.forEach(titleElements => {
+// titleElements.style.color = "blue";
+// });
+//
+// //jQuery to select and change all li elements
+// $('li').css('color', 'pink');
+//
+// //multiple selectors jQuery
+// $('h1, p, .highlight').css('font-weight', 'bold');
+//
+// //all selectors jQuery
+// $('*')
+//     .css('outline', '1px solid lightblue')
+//     .css('font-weight', 'bold');
+
+// ID jQuery Exercise
+// let paragraph1Content = $('#paragraph1').html();
+// alert(paragraph1Content);
+
+// Classes jQuery Exercise
+// $('.codeup').css('border', '1px solid red');
+
+//Element jQuery Exercise
+// $('li').css('font-size', '20px');
+// let h1Alert = $('h1').html();
+// alert(h1Alert);
+
+//Multiple selectors jQuery Exercise
+// $('h1, p, li').css('background-color', 'yellow');
+
+
