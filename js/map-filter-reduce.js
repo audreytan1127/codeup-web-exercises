f0a012a37c91449de339d72647e02f284198b748
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-07-21 09:54:23 -0500

    completed class walk thru on map-filter-reduce.js

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -41,7 +41,11 @@ const userLanguages = users.filter(user=> {
 const userLanguageBank = user.languages;
 return userLanguageBank.length >= 3;
 });
-console.log(userLanguages);
+console.log('Users with three languages: ',userLanguages);
+
+// CLASS WALK THRU
+// const threeLang = users.filter(user => user.language.length >= 3);
+// console.log(threeLang);
 
 // ////////////////EXERCISE 2
 //Use .map to create an array of strings where each element is a user's email address
@@ -53,67 +57,123 @@ const userEmails = users.map((user)=>{
 console.log(userEmails);
 
 
+// CLASS WALK THRU
+// const emailArray = users.map(user => user.email);
+// console.log('Users emails: ', emailArray);
+
+
 // ////////////////EXERCISE 3
 //Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
 
 //store the total amount of users in variable
-let totalUsers = users.length;
-//set the years of experience to 0 to add years of exp. each user has
-let yearsOfExperience = 0;
-// declare reduce method
-const averageYears = users.reduce((accumulator, user, index)=>{
-    // add each users years of experience to the counter
-yearsOfExperience += user.yearsOfExperience;
-//set the total years of experience of all users to the accumulator object value
-        accumulator.totalYearsOfExperience = yearsOfExperience;
-        //set the average years of experience users have to accumulator object
-        accumulator.averageYearsOfExperience = yearsOfExperience / totalUsers;
-        //return the accumulator object
-        return accumulator;
+// let totalUsers = users.length;
 
-    },
-//set the accumulator object to store values to calculate average
-    {
-       totalYearsOfExperience: 0,
-        averageYearsOfExperience: 0
-    });
-console.log(averageYears);
+// //set the years of experience to 0 to add years of exp. each user has
+// let yearsOfExperience = 0;
+
+// // declare reduce method
+// const averageYears = users.reduce((accumulator, user, index)=>{
+
+//     // add each users years of experience to the counter
+// yearsOfExperience += user.yearsOfExperience;
+
+// //set the total years of experience of all users to the accumulator object value
+//         accumulator.totalYearsOfExperience = yearsOfExperience;
+
+//         //set the average years of experience users have to accumulator object
+//         accumulator.averageYearsOfExperience = yearsOfExperience / totalUsers;
+
+//         //return the accumulator object
+//         return accumulator;
+//
+//     },
+// //set the accumulator object to store values to calculate average
+//     {
+//        totalYearsOfExperience: 0,
+//         averageYearsOfExperience: 0
+//     });
+// console.log(averageYears);
+
+
+// // CLASS WALK THRU
+// const avgYears = users.reduce((accumulator, user,index)=> {
+//     accumulator += parseFloat(user.yearsOfExperience);
+// //Ability to run conditional in loop, this will run average at the end of loop!!
+//     if(users.length-1 ===index) {
+//         accumulator = accumulator/users.length;
+//     }
+//     return accumulator;
+// }, 0);
+// console.log('Average Years: ', avgYears);
+
+// COULD ALSO LOG THE TOTAL YEARS AND DIVIDE BY USERS.LENGTH!
 
 
 // ///////////////EXERCISE 4
 //Use .reduce to get the longest email from the list of users.
 //set the counter for the number of characters in the user email to 0
-    let longestUserEmail = 0;
-    //use reduce to find the longest email
-const longestEmail = users.reduce((accumulator, user, index)=>{
-    //check if all of the user email lengths are more than the longest user email
-    if(longestUserEmail < user.email.length) {
-        //set the longest user email length to the variable.
-        longestUserEmail = user.email.length;
-        // the email that is the longest will have the amount of characters placed into the accumulator property totalCharacters.
-        accumulator.totalCharacters = longestUserEmail;
-        //the email that is the longest will be printed into the accumulator property userEmail.
-        accumulator.userEmail = user.email;
-    }
-    //return the accumulator value
-       return accumulator;
-}, {
-    // declare the accumulator values to return from reduce function
-    totalCharacters: 0,
-    userEmail: users[0].email
-})
-// call function
-console.log(longestEmail);
+//     let longestUserEmail = 0;
+//
+//     //use reduce to find the longest email
+// const longestEmail = users.reduce((accumulator, user, index)=>{
+//
+//     //check if all of the user email lengths are more than the longest user email
+//     if(longestUserEmail < user.email.length) {
+//
+//         //set the longest user email length to the variable.
+//         longestUserEmail = user.email.length;
+//
+//         // the email that is the longest will have the amount of characters placed into the accumulator property totalCharacters.
+//         accumulator.totalCharacters = longestUserEmail;
+//
+//         //the email that is the longest will be printed into the accumulator property userEmail.
+//         accumulator.userEmail = user.email;
+//     }
+//     //return the accumulator value
+//        return accumulator;
+// }, {
+//     // declare the accumulator values to return from reduce function
+//     totalCharacters: 0,
+//     userEmail: users[0].email
+// })
+//
+// // call function
+// console.log(longestEmail);
 
+
+//CLASS WALK THRU
+const longestEmail = users.reduce((accumulator, user, index)=> {
+    const emailLength = user.email.length;
+    const accLength = accumulator.length;
+    if(emailLength > accLength) {
+        accumulator = user.email;
+    }
+    return accumulator;
+}, "");
+console.log('The longest email is: ', longestEmail);
 
 // ///////////////EXERCISE 5
 //Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+//
+// ///////////This just concatenates all the user names together with a comma between....
+// const userNames = users.reduce((accumulator, user, index)=> {
+//     //if at the index 0 of the user.names doesn't have the user.name, add the user.name to the accumulator and separate with a comma.
+//     return index==0 ? user.name : accumulator + ', ' + user.name;
+// }, '');
+// console.log(userNames);
 
-const userNames = users.reduce((accumulator, user, index)=> {
-    //if at the index 0 of the user.names doesn't have the user.name, add the user.name to the accumulator and separate with a comma.
-    return index==0 ? user.name : accumulator + ', ' + user.name;
-}, '');
-console.log(userNames);
+// /////////////////This concatenates the usernames together with comma other than last one which has and, and a period before.
+// // CLASS WALK THRU
+const usersString = users.reduce((accumulator, user, index)=>{
+if(index ===(users.length-1)) {
+    accumulator += `and ${user.name}.`;
+} else {
+    accumulator += `${user.name}, `;
+}
+return accumulator;
+}, "Your instructors are:");
+console.log(usersString);
+
 
 
 // ////////////////BONUS REDUCE
